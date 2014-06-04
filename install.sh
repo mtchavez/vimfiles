20eb12339d1527f46e6ed9319b11c0ecacd556a3
Chavez <nailsbrokeandfell@gmail.com>
2014-06-04 11:20:38 -0700

    Fix install

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 # install vundle
 echo "Installing vundle..."
-  if [ -d "vim/bundle/vundle" ]; then
-    cd "vim/bundle/vundle"
+  if [ -d ".vim/bundle/vundle" ]; then
+    pushd .vim/bundle/vundle
     git pull
-    cd "../../.."
+    popd
   else
-    git clone http://github.com/gmarik/vundle.git vim/bundle/vundle
+    git clone http://github.com/gmarik/vundle.git .vim/bundle/vundle
   fi
 echo "Done installing vundle."
 
 echo "Rsync vimfiles over"
-rsync --exclude ".git" --exclude "install.sh" --exclude "LICENSE" \
+rsync --exclude ".git" --exclude "install.sh" --exclude "LICENSE" --exclude "*.un~" \
     --exclude "README.md" --exclude ".DS_Store" -av --no-perms . ~
 
-vim -c BundleInstall! -c BundleClean! -c q -c q
+mvim -c BundleInstall! -c BundleClean! -c q -c q
 
 echo
 echo "Installed successfully."

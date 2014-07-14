c3f195e00e96bccfd88b83f8ddc6d6fbb90bb3d1
Chavez <nailsbrokeandfell@gmail.com>
2014-07-14 07:57:56 -0700

    Install updates for vundle changes

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 # install vundle
 echo "Installing vundle..."
-  if [ -d ".vim/bundle/vundle" ]; then
-    pushd .vim/bundle/vundle
+  if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+    pushd ~/.vim/bundle/Vundle.vim
     git pull
     popd
   else
-    git clone http://github.com/gmarik/vundle.git .vim/bundle/vundle
+    git clone http://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
   fi
 echo "Done installing vundle."
 
@@ -15,7 +15,7 @@ echo "Rsync vimfiles over"
 rsync --exclude ".git" --exclude "install.sh" --exclude "LICENSE" --exclude "*.un~" \
     --exclude "README.md" --exclude ".DS_Store" -av --no-perms . ~
 
-mvim -c BundleInstall! -c BundleClean! -c q -c q
+mvim -c PluginInstall! -c PluginClean! -c q -c q
 
 echo
 echo "Installed successfully."

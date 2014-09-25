dcad832f17bf77576f61947530d0f8cffe6cba58
Chavez <nailsbrokeandfell@gmail.com>
2014-09-25 10:45:13 -0700

    Add option to update plugins or not when installing

--- install.sh
+++ install.sh
@@ -2,23 +2,36 @@
 
 # install vundle
 echo "Installing vundle..."
-  if [ ! -d "~/vim/bundle" ]; then
-      mkdir -p ~/.vim/bundle
-  fi
-  if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+if [ ! -d "~/vim/bundle" ]; then
+    mkdir -p ~/.vim/bundle
+fi
+if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
     git clone http://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
-  else
+else
     pushd ~/.vim/bundle/Vundle.vim
     git pull
     popd
-  fi
+fi
 echo "Done installing vundle."
 
 echo "Rsync vimfiles over"
 rsync --exclude ".git" --exclude ".gitignore" --exclude "install.sh" --exclude "LICENSE" --exclude "*.un~" \
     --exclude "README.md" --exclude ".DS_Store" -av --no-perms . ~
 
-mvim -c PluginInstall! -c PluginClean! -c q -c q
+while getopts "u" option
+do
+    case "$option" in
+    u)
+        UPDATE=1
+        ;;
+    esac
+done
+
+if [ -n "$UPDATE" ]; then
+    mvim -c PluginInstall! -c PluginClean! -c q -c q
+else
+    echo "Skipping plugin update; pass -u if you want to update plugins"
+fi
 
 echo
 echo "Installed successfully."

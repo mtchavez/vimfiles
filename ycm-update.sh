25fc846440fa1c27b58303a3926804f0df200daf
Chavez <matthew@el-chavez.me>
2015-05-28 16:03:14 -0700

    Short-circuit ycm-update if not installed yet

--- ycm-update.sh
+++ ycm-update.sh
@@ -6,6 +6,12 @@
 YCM_DIR=$HOME/.vim/bundle/YouCompleteMe
 YCM_OPTS="--clang-completer --gocode-completer"
 
+if [ ! -d $YCM_DIR ]
+then
+    echo "Please bundle YouCompleteMe before attempting to install/update"
+    exit 0
+fi
+
 pushd $YCM_DIR > /dev/null
     if [ ! -f $HOME/.ycm_installed ]; then
         ./install.sh $YCM_OPTS

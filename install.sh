b8dd7217ab8856e79d976ffabedb6948b459cd76
Chavez <matthew@el-chavez.me>
2015-05-28 08:24:55 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 #
 # install vundle
-echo "Installing vundle..."
-if [ ! -d "~/vim/bundle" ]; then
-    mkdir -p ~/.vim/bundle
+echo "----> Installing vundle..."
+echo
+if [ ! -d "$HOME/.vim/bundle" ]; then
+    mkdir -p $HOME/.vim/bundle
 fi
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
-    git clone http://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+    git clone http://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim
 else
-    pushd ~/.vim/bundle/Vundle.vim
+    pushd $HOME/.vim/bundle/Vundle.vim
     git pull
     popd
 fi
-echo "Done installing vundle."
+echo
+echo "----> Done installing vundle."
+echo
 
-echo "Rsync vimfiles over"
-rsync --exclude ".git" --exclude ".gitignore" --exclude "install.sh" --exclude "LICENSE" --exclude "*.un~" \
-    --exclude "README.md" --exclude ".DS_Store" -av --no-perms . ~
+echo "----> Rsync vimfiles over"
+echo
+rsync --exclude ".git" --exclude ".gitignore" --exclude "/.vim/bundle"--exclude "install.sh" --exclude "ycm-update.sh" --exclude "*.un~" \
+    --exclude "README.md" --exclude ".DS_Store" -avn --no-perms . ~
+
+# Ensure bundle dir
+mkdir -p $HOME/.vim/bundle
 
 while getopts "u" option
 do
@@ -30,9 +37,10 @@ done
 if [ -n "$UPDATE" ]; then
     mvim -c PluginInstall! -c PluginClean! -c q -c q +qall
 else
-    echo "Skipping plugin update; pass -u if you want to update plugins"
+    echo
+    echo "----> Skipping plugin update; pass -u if you want to update plugins"
 fi
 
 echo
-echo "Installed successfully."
+echo "----> Installed successfully."
 echo
